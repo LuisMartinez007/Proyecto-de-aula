edc32677df99f8f2b150ea0912ab986bf8b740a1
Luis <luis.martinezlopez@unicolombo.edu.co>
2024-05-14 19:04:25 -0500

    Nuevo repositorio

---
+++ Restaurante/src/co/edu/unicolombo/pb/proaula/Principal.java
@@ -0,0 +1,23 @@
+
+package co.edu.unicolombo.pb.proaula;
+
+import co.edu.unicolombo.pb.proaula.views.VentanaPrincipal;
+
+
+
+public class Principal {
+
+    
+    public static void main(String[] args) {
+        
+    VentanaPrincipal ventana = new VentanaPrincipal();
+        
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
+       
+    }
+    
+    
+    
+    
+}
